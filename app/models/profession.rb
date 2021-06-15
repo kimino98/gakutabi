3b6520248d790f0e8c0404520ae4a72a64c0c0b2
kimino98 <k.t.ynwa0105@gmail.com>
2021-06-15 19:27:41 +0900

    カテゴリー記述

--- app/models/profession.rb
+++ app/models/profession.rb
@@ -1,2 +1,10 @@
-class Profession < ApplicationRecord
+class Profession < ActiveHash::Base
+  self.date = [
+    { id: 1, name: '--'},
+    { id: 2, name: '小学校教諭'},
+    { id: 3, name: '中学校教諭'},
+    { id: 4, name: '高等学校教諭'},
+    { id: 5, name: '特別支援学校教諭'},
+    { id: 6, name: '旅行会社'},
+  ]
 end
