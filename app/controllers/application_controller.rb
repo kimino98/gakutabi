dc37a6f312f1f4c61869275a6282ad363f9a54e9
kimino98 <k.t.ynwa0105@gmail.com>
2021-06-17 18:16:32 +0900

    ユーザーの条件分岐

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :company_name, :profession_id])
-  end
-
-
+ 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :company_name, :profession_id])
