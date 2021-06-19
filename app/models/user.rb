ddfead95eeeafc2b8cb23034ec5a00ffb0195acb
kimino98 <k.t.ynwa0105@gmail.com>
2021-06-19 23:52:58 +0900

    半角英数字を両方含む6バリデーションに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname, length: { maximum: 10 }
-    validates :password, format: { with: /\A[a-z0-9]+\z/ }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message:'は半角英数字を両方含む6文字にして下さい' }
     validates :company_name
     validates :profession_id, numericality: { other_than: 1, message: 'を選択して下さい'}
   end
