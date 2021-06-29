8bb52fbe2ca5fd57f92a421a64bc52707d44a678
kimino98 <k.t.ynwa0105@gmail.com>
2021-06-29 18:38:04 +0900

    ホテルタイプactivehash記述

---
+++ app/models/hotel_type.rb
@@ -0,0 +1,20 @@
+class Hotel_type < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '宿泊施設ではない' },
+    { id: 3, name: 'ホテル' },
+    { id: 4, name: '旅館' },
+    { id: 5, name: 'コテージ' },
+    { id: 6, name: 'メゾネット' },
+    { id: 7, name: 'アネックス' },
+    { id: 8, name: '民泊' },
+    { id: 9, name: 'キャンプ' },
+    { id: 10, name: '農家民泊' },
+    { id: 11, name: '国民宿舎' },
+    { id: 12, name: '休暇村' },
+    { id: 13, name: 'その他' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :reviews
+end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :hotel_type
 end
