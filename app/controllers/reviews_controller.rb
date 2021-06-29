4f927bf982b888025ec6d2909513574dd7d4afd7
kimino98 <k.t.ynwa0105@gmail.com>
2021-06-29 16:31:53 +0900

    newアクション記述

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,8 @@ class ReviewsController < ApplicationController
   def index
     @reviews = Review.all
   end
+
+  def  new
+    @reviews = Review.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "reviews#index"
-  resources :reviews, only: :index
+  resources :reviews, only: [:index, :new]
 end
