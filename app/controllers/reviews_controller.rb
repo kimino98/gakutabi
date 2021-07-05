8f1ab6fdbbd5736f532c06a55fadd1115acb66ec
kimino98 <k.t.ynwa0105@gmail.com>
2021-07-05 22:22:23 +0900

    投稿した内容をDBに保存

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,22 @@ class ReviewsController < ApplicationController
     @reviews = Review.all
   end
 
-  def  new
-    @reviews = Review.new
+  def new
+    @review = Review.new
   end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to root_path
+     else
+      render :new
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:faclity_name, :hotel_type_id, :grade_id, :season_id, :region_id, :student_count_id, :price, :text, :safety, images: []).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "reviews#index"
-  resources :reviews, only: [:index, :new]
+  resources :reviews, only: [:index, :new, :create]
 end
