b43fa1c1daf9ef63c8bc24ce50496ed8c728271b
kimino98 <k.t.ynwa0105@gmail.com>
2021-07-07 22:32:05 +0900

    reviewモデル単体テストコード

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :review do
-    
+    faclity_name     {Faker::Company.name}
+    hotel_type_id    {Faker::Number.within(range: 2..13)}
+    grade_id         {Faker::Number.within(range: 2..20)}
+    region_id        {Faker::Number.within(range: 2..47)}
+    student_count_id {Faker::Number.within(range: 2..12)}
+    season_id        {Faker::Number.within(range: 2..5)}
+    price            {Faker::Number.number}
+    text             {Faker::Lorem.sentence}
+    safety           {Faker::Lorem.sentence}
+    association :user
+
+    after(:build) do |review|
+      review.images.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,97 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @review = FactoryBot.build(:review)
+  end
+
+  describe '投稿機能' do
+    context '投稿できるとき' do
+      it '全ての情報が正しく入力されいるとき' do
+        expect(@review).to be_valid
+      end
+    end
+
+    context '投稿が失敗するとき' do
+      it 'faclity_nameが空だと投稿できない' do
+        @review.faclity_name = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include('施設名を入力してください')
+      end
+
+      it 'faclity_nameが40文字以上だと投稿できない' do
+        @review.faclity_name = '東京遊園地' * 40
+        @review.valid?
+        expect(@review.errors.full_messages).to include('施設名は40文字以内で入力してください')
+      end
+
+      it 'hotel_type_idが空の場合投稿できない' do
+        @review.hotel_type_id = ''
+        @review.valid?
+        expect(@review.errors.full_messages).to include('宿泊タイプを選択して下さい')
+      end
+
+      it 'hotel_type_idが1を場合投稿できない' do
+        @review.hotel_type_id = 1
+        @review.valid?
+        expect(@review.errors.full_messages).to include('宿泊タイプを選択して下さい')
+      end
+
+      it 'grade_idが空の場合投稿できない' do
+        @review.grade_id = ''
+        @review.valid?
+        expect(@review.errors.full_messages).to include('学年を選択して下さい')
+      end
+
+      it 'grade_idが1の場合投稿できない' do
+        @review.grade_id = 1
+        @review.valid?
+        expect(@review.errors.full_messages).to include('学年を選択して下さい')
+      end
+
+      it 'season_idが空の場合投稿できない' do
+        @review.season_id = ''
+        @review.valid?
+        expect(@review.errors.full_messages).to include('季節を選択して下さい')
+      end
+
+      it 'season_idが1の場合投稿できない' do
+        @review.season_id = 1
+        @review.valid?
+        expect(@review.errors.full_messages).to include('季節を選択して下さい')
+      end
+
+      it 'region_idが空の場合投稿できない' do
+        @review.region_id = ''
+        @review.valid?
+        expect(@review.errors.full_messages).to include('施設所在地を選択して下さい')
+      end
+
+      it 'region_idが1の場合投稿できない' do
+        @review.region_id = 1
+        @review.valid?
+        expect(@review.errors.full_messages).to include('施設所在地を選択して下さい')
+      end
+
+      it 'student_count_idが空の場合投稿できない' do
+        @review.student_count_id = ''
+        @review.valid?
+        expect(@review.errors.full_messages).to include('生徒人数を選択して下さい')
+      end
+
+      it 'student_count_idが1の場合投稿できない' do
+        @review.student_count_id = 1
+        @review.valid?
+        expect(@review.errors.full_messages).to include('生徒人数を選択して下さい')
+      end
+
+      it 'textが140文字以上だと投稿できない' do
+        @review.text = 'テストです' * 140
+        @review.valid?
+        expect(@review.errors.full_messages).to include('感想は140文字以内で入力してください')
+      end
+
+
+    end
+  end
 end
