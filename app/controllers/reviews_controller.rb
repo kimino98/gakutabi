f9ebf37570c69c262d893ea909b9257b8b652824
kimino98 <k.t.ynwa0105@gmail.com>
2021-07-16 22:40:41 +0900

    削除機能記述

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,6 +17,17 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    if @review.destroy
+      flash[:notice] = '削除しました'
+      redirect_to root_path
+     else
+      flash[:alert] = '削除に失敗しました'
+      redirect_to root_path
+     end
+  end
+
   private
   def review_params
     params.require(:review).permit(:faclity_name, :hotel_type_id, :grade_id, :season_id, :region_id, :student_count_id, :price, :text, :safety, images: []).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "reviews#index"
-  resources :reviews, only: [:index, :new, :create, :show]
+  resources :reviews, only: [:index, :new, :create, :show, :destroy]
 end
