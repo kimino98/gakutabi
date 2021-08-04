6c07ee7990a0bf4b3f3cb9d320d39d913f084661
kimino98 <k.t.ynwa0105@gmail.com>
2021-08-04 21:51:21 +0900

    likeコントローラー記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,22 @@
 class LikesController < ApplicationController
-  before_action :set_review
+  before_action :set_like
 
   def create
-    @like = Like.cretate(user_id: current_user.id, review_id: review.id)
+    user = current_user
+    review = Review.find(params[:review_id])
+    like = Like.create(user_id: user.id, review_id: review.id)
   end
 
   def destroy
-    @like = current_user.liles.find_by(review_id: @review.id)
+    user = current_user
+    review = Review.find(params[:review_id])
+    like = Like.find_by(user_id: user.id, review_id: review.id)
+    like.destroy
   end
 
   private
 
-  def set_review
+  def set_like
     @review = Review.find(params[:review_id])
   end
 end
