edb5f8713d280301b475f08fe08164cd95ce1d6a
kimino98 <k.t.ynwa0105@gmail.com>
2021-08-11 16:42:29 +0900

    ユーザー編集ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,32 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
+    
     @reviews = @user.reviews.page(params[:page]).per(10).order('updated_at DESC')
     @likes = Like.where(user_id: @user.id)
   end
+
+  def edit
+    
+  end
+
+  def update
+    if current_user.update(user_params)
+      flash[:notice] = '編集に成功しました'
+      redirect_to user_path(current_user)
+     else
+      flash[:alert] = '編集に失敗しました'
+      redirect_to edit_user_path(current_user)
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:nickname, :email, :company_name, :profession_id, :image)
+    end
 end
