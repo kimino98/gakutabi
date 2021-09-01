605835413ff23aa5616be3845c526bb465625310
kimino98 <k.t.ynwa0105@gmail.com>
2021-09-01 16:00:45 +0900

    いいね機能単体テストコード記述

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @like = FactoryBot.build(:like)
+  end
+  describe 'いいねをする' do
+    context 'いいねが保存される時' do
+      it '正しい情報が存在する時、言い値は保存される' do
+        expect(@like).to be_valid
+      end
+    end
+    context 'いいねが保存されない時' do
+      it 'ユーザが紐づいていないと保存されない' do
+        @like.user = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include('Userを入力してください')
+      end
+      it '投稿が紐づいていないと保存されない' do
+        @like.review = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include('Reviewを入力してください')
+      end
+    end
+  end
 end
