e8cdc45e526e727cefc56a65751b45701d512470
kimino98 <k.t.ynwa0105@gmail.com>
2021-09-03 16:57:10 +0900

    投稿編集・削除結合テスト記述

--- spec/system/reviews_spec.rb
+++ spec/system/reviews_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe "新規投稿", type: :system do
       fill_in 'review[safety]', with: @review.safety
       image_path = Rails.root.join('public/images/test_image.png')
       attach_file('review[images][]', image_path, make_visible: true)
-      # 送信するとTweetモデルのカウントが1上がることを確認する
+      # 送信するとreviewモデルのカウントが1上がることを確認する
       expect{
         find('input[name="commit"]').click
       }.to change { Review.count }.by(1)
@@ -74,9 +74,10 @@ RSpec.describe "投稿編集", type: :system do
   before do
     @user = FactoryBot.create(:user)
     @review = FactoryBot.create(:review, user_id: @user.id)
+    @another = FactoryBot.create(:user)
   end
   context '投稿編集ができるとき'do
-    it 'ログイン後ユーザーは自分の投稿の編集ボタンを押して編集ページに遷移し、正しく入力すると投稿が編集される' do
+    it 'ログイン後自分の投稿詳細ページから編集ボタンを押して編集ページに遷移し、正しく入力すると投稿が編集される' do
       # ログインする
       sign_in(@user)
       # 自分の投稿であることを確認する
@@ -105,6 +106,91 @@ RSpec.describe "投稿編集", type: :system do
       expect(current_path).to eq(root_path)
       # 編集された箇所が表示されている事を確認する
       expect(page).to have_content('変更')
+    end
+  end
+  context '投稿編集できないとき' do
+    it 'ログイン後、投稿詳細ページから編集ボタンを押して編集ページに遷移し、誤った入力すると編集できない' do
+      # ログインする
+      sign_in(@user)
+      # 自分の投稿であることを確認する
+      expect(page).to have_content(@review.faclity_name)
+      # 詳細ページに遷移
+      visit review_path(@review)
+      # 編集ボタン確認
+      expect(page).to have_content('編集する')
+      # 編集ページへ遷移
+      visit edit_review_path(@review)
+      # 編集内容情報を入力する
+      fill_in 'review[faclity_name]', with: ''
+      fill_in 'review[price]', with: ''
+      fill_in 'review[text]', with: ''
+      fill_in 'review[safety]', with: ''
+      # 編集完了ボタンを押す
+      find('input[name="commit"]').click
+    end
+    it '自分以外の投稿は編集ボタンがないのでできない' do
+      # 別のユーザーでサインイン
+      sign_in(@another)
+      # 詳細ページに遷移
+      visit review_path(@review)
+      # 編集ボタンがないことを確認
+      expect(page).to have_no_content('編集する')
+    end
+    it 'ログインしないと詳細ページには遷移できるが編集ページに遷移することはできない' do
+      # トップページ
+      visit root_path
+      # 詳細ページに遷移
+      visit review_path(@review)
+      # 編集ボタンがないことを確認
+      expect(page).to have_no_content('編集する')
+    end
+  end
+end
+
+describe '投稿削除' do
+  before do
+    @user = FactoryBot.create(:user)
+    @review = FactoryBot.create(:review, user_id: @user.id)
+    @another = FactoryBot.create(:user)
+  end
+  context '投稿削除できるとき' do
+    it 'ログイン後、投稿詳細ページから自分の投稿を削除できる' do
+      # ログインする
+      sign_in(@user)
+      # 自分の投稿であることを確認する
+      expect(page).to have_content(@review.faclity_name)
+      # 詳細ページに遷移
+      visit review_path(@review)
+      # 削除ボタン確認
+      expect(page).to have_content('削除する')
+      # 投稿を削除するとreviewモデルのカウントが1下がることを確認する
+      expect{
+        click_on('削除する')
+        expect(page.accept_confirm).to eq '本当に削除しますか?'
+        sleep 0.5
+      }.to change { Review.count }.by(-1)
+      # 投稿一覧ページに遷移
+      expect(current_path).to eq(root_path)
+      # 削除しましたを確認
+      expect(page).to have_content('削除しました')
+    end
+  end
+  context '投稿削除できないとき' do
+    it '自分以外の投稿は削除ボタンがないのでできない' do
+      # 別のユーザーでサインイン
+      sign_in(@another)
+      # 詳細ページに遷移
+      visit review_path(@review)
+      # 編集ボタンがないことを確認
+      expect(page).to have_no_content('削除する')
+    end
+    it 'ログインしないと詳細ページには遷移できるが削除することはできない' do
+       # トップページ
+       visit root_path
+       # 詳細ページに遷移
+       visit review_path(@review)
+       # 編集ボタンがないことを確認
+       expect(page).to have_no_content('削除する')
     end
   end
 end
